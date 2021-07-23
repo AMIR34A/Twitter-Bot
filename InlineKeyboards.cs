29b8d2a6d23674c170c25e5a42837031e13712c2
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2021-07-23 20:10:11 +0430

    Add files via upload

---
+++ InlineKeyboards.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TwitterBot.Classes
+{
+    class InlineKeyboards
+    {
+        public InlineKeyboardMarkup ShowDetail(int likeCount, int? retweetCount, string url)
+        {
+            var keyboard = new InlineKeyboardButton[][]
+            {
+                new InlineKeyboardButton[]
+                {
+                     InlineKeyboardButton.WithCallbackData($"🔄{retweetCount}") , InlineKeyboardButton.WithCallbackData($"❤️{likeCount}")
+                }
+            };
+            return new InlineKeyboardMarkup(keyboard);
+        }
+    }
+}
