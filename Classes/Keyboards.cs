ed1d7c182cb2e4e2f20deadb292c9599d028cff9
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2021-08-11 16:45:25 +0430

    Add files via upload

---
+++ Classes/Keyboards.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TwitterBot.Classes
+{
+    class Keyboards
+    {
+        #region InlineKeyBoards
+        public InlineKeyboardMarkup ShowDetail(int likeCount, int? retweetCount, string url)
+        {
+            var keyboard = new InlineKeyboardButton[][]
+            {
+                new InlineKeyboardButton[]
+                {
+                     InlineKeyboardButton.WithCallbackData($"🔄{retweetCount}") , InlineKeyboardButton.WithCallbackData($"❤️{likeCount}")
+                }
+            };
+            return new InlineKeyboardMarkup(keyboard);
+        }
+        #endregion
+
+        #region ReplyKeyboards
+        public ReplyKeyboardMarkup ReplyKeyboard()
+        {
+            var keyboards = new KeyboardButton[][]
+            {
+                new[] { new KeyboardButton("⚙️Settings") }
+            };
+            return new ReplyKeyboardMarkup(keyboards) { ResizeKeyboard = true };
+        }
+        #endregion
+    }
+}
