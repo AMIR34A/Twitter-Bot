b8f026133e4de0423e61b32f38c13e98a3ac2e73
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2021-08-21 18:20:51 +0430

    Add files via upload

---
+++ Classes/Handler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TwitterBot.Classes
+{
+    class Handler
+    {
+        Action<Telegram.Bot.Types.Update, Telegram.Bot.TelegramBotClient> responce;
+        Methods method = new Methods();
+        public Handler(Telegram.Bot.Types.Update update, Telegram.Bot.TelegramBotClient bot)
+        {
+            if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                switch (update.Message.Text.ToLower())
+                {
+                    case "/start":
+                        responce += method.ResponceToStart;
+                        break;
+
+                    case "/trends":
+                        responce += method.GetTrends;
+                        break;
+
+                    case "⚙️Settings":
+                        break;
+
+                    default:
+                        if (update.Message.Text.Contains("https://twitter.com/") || update.Message.Text.Contains("http://twitter.com/"))
+                            responce += method.GetTweet;
+                        break;
+                }
+
+                if (responce != null)
+                    responce(update, bot);
+            }
+        }
+    }
+}
---
+++ Classes/Process.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TwitterBot.Classes
+{
+    class Process
+    {
+        public string Country { get; set; }
+        TwitterClient userClient = new TwitterClient("zAra0T5tcxYqoZzGbzNn8zB1h", "2VUh6nBQO2OX7GHDMPLWaxmRziXrZyjaUWOn0COVtIP6MxoV8O", "1297246709253394432-HZd5WrvERipQyVNwcmsYS7kGrikeje", "FhNO7jinFZmHKPyJoRLnAsDXmK8QWK9VF6NwnSz38myXO");
+
+        #region GetTweet
+        public bool GetTweet(ref string screenName, ref string text, ref string name, string id, ref int likeCount, ref int retweetCount, Telegram.Bot.Types.Update update, Telegram.Bot.TelegramBotClient bot)
+        {
+            bool valid = true;
+            //userClient.Config.ProxyConfig = new ProxyConfig("http://fast.iproxypersonalproxy.club")
+            //{
+            //    Credentials = new NetworkCredential("3dljg25v", "q1kHa7fZ")
+            //};
+            try
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                var tweet = userClient.Tweets.GetTweetAsync(long.Parse(id));
+                screenName = tweet.Result.CreatedBy.ScreenName;
+                text = tweet.Result.FullText;
+                name = tweet.Result.CreatedBy.Name;
+                likeCount = tweet.Result.FavoriteCount;
+                retweetCount = tweet.Result.RetweetCount;
+            }
+            catch
+            {
+                valid = false;
+            }
+            return valid;
+        }
+        #endregion 
+
+        #region GetTrends
+        public async Task<List<ITrend>> GetTrends(double latitude = 32.5176, double longitude = 59.1042)
+        {
+            var coordinates = new Coordinates(latitude, longitude);
+            var trendingLocations = await userClient.Trends.GetTrendsLocationCloseToAsync(coordinates);
+            var trends = await userClient.Trends.GetPlaceTrendsAtAsync(trendingLocations[0].WoeId);
+            var result = trends.Trends.Where((t, i) => i <= 4).Select(t => t).ToList();
+            Country = trendingLocations[0].Country;
+            return await Task.FromResult(result);
+        }
+        #endregion
+    }
+}
